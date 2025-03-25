9af3bb841587b4e1de39afba8c3c2b472acca85b
Destomerito <destomerito@gmail.com>
2025-03-25 08:03:22 -0700

    removed pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,33 +23,7 @@ export default defineNuxtConfig({
       { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
     ]
   },
-  pwa: {
-    manifest: {
-      name: 'Motion Express Way',
-      short_name: 'MotionExpressWay',
-      description: 'Track Your Shipment',
-      theme_color: '#ffffff',
-      background_color: '#ffffff',
-      display: 'standalone',
-      icons: [
-        {
-          src: '/images/logo.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/images/logo.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workbox: {
-      navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,png,svg,ico,json}']
-    },
-    registerType: 'autoUpdate'
-  },
+ 
   
   compatibilityDate: '2025-03-20',
 });
